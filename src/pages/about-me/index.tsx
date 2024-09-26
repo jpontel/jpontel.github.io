5bc7ef2f1d70191fa4b49c205a5bc9ba0cafc137
jpontel <jpontel2002@gmail.com>
2024-09-26 16:39:59 -0300

    mockup MOBILE

--- src/pages/about-me/index.tsx
+++ src/pages/about-me/index.tsx
@@ -11,9 +11,11 @@ export default function AboutMe() {
                     <div className={'font-baijamjuree font-bold xsm:text-[30px] md:text-[50px] w-full text-white'}>
                         José Rodrigo Pontel
                     </div>
-                    <div className={'text-left xsm:text-[15px] leading-8 md:text-[18px] xsm:w-full md:w-2/3 text-white'}>
-                        I'm currently looking to join a cross-functional team that values improving people's lives
-                        through accessible design
+                    <div
+                        className={'text-left xsm:text-[15px] leading-8 md:text-[18px] xsm:w-full md:w-2/3 text-white'}>
+                        I am looking to join a cross-functional team where I can leverage my skills in front-end and
+                        back-end development, UI/UX design, and machine learning to create impactful and accessible
+                        digital experiences.
                     </div>
                 </div>
                 <div className={'w-full justify-start items-start flex gap-12 mb-12'}>
@@ -24,8 +26,10 @@ export default function AboutMe() {
             <div className={'flex flex-col w-full h-full items-center justify-center font-baijamjuree'}>
                 <Carousel/>
                 <div className={'w-full h-full flex items-center justify-center mt-12 font-baijamjuree'}>
-                    <div className={'w-full h-[400px] styled-background rounded-b-[50px] flex flex-col items-start font-baijamjuree font-bold text-center py-12'}>
-                        <span className={'w-full h-full xsm:p-2 md:p-0 flex flex-col items-center justify-center xsm:text-[45px] md:text-[60px] text-white'}>
+                    <div
+                        className={'w-full h-[400px] styled-background rounded-b-[50px] flex flex-col items-start font-baijamjuree font-bold text-center py-12'}>
+                        <span
+                            className={'w-full h-full xsm:p-2 md:p-0 flex flex-col items-center justify-center xsm:text-[45px] md:text-[60px] text-white'}>
                             Interested in working with me?
                         </span>
                         <span className={'w-full h-full flex items-center justify-center'}>
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,6 +1,6 @@
 import ProjectsBlock from "@/components/projects-block";
 import weatherApp from '@/assets/weatherapp-mockup.png';
-import aguadoce from  '@/assets/aguadoce-png.png';
+import aguadoce from '@/assets/aguadoce-png.png';
 import picture from '@/assets/prisoner.png';
 import './../home/style/style.css';
 import {isMobile} from "@/util";
@@ -12,7 +12,8 @@ export default function Projects() {
     return (
         <div className={'mb-32 xsm:px-[20px] md:p-32 pt-32 flex flex-col gap-20 md:px-[250px]'}>
             <div className={'text-white justify-start items-start flex flex-col'}>
-                <div className={'font-baijamjuree xsm:text-[30px] md:text-[50px] font-bold xsm:w-full md:w-[350px] mb-[35px]'}>
+                <div
+                    className={'font-baijamjuree xsm:text-[30px] md:text-[50px] font-bold xsm:w-full md:w-[350px] mb-[35px]'}>
                     What I've been <span className={'styled-text'}>working on?</span>
                 </div>
                 <div className={'text-left xsm:text-[15px] leading-8 md:text-[18px] xsm:w-full md:w-2/3'}>
@@ -22,21 +23,47 @@ export default function Projects() {
                 </div>
             </div>
             {mobile ? (
-                <div></div>
+                <div className={'flex flex-col w-full h-full gap-8'}>
+                    <ProjectsBlock title={'Weather App'}
+                                   description={'Mobile App created using Swift for weather prediction using AccuWeather API'}
+                                   img={weatherApp}
+                                   size={'small'} width={'w-full'}/>
+                    <ProjectsBlock title={'Vegan Recipes'}
+                                   description={'Mobile App created using Flutter to bring easily vegan recipes for users.'}
+                                   img={picture}
+                                   size={'small'} width={'w-full'}/>
+                    <ProjectsBlock title={'E-Commerce Webstore'}
+                                   description={'Clothing webstore made with React Typescript'} img={aguadoce}
+                                   size={'large'} width={'w-full'}/>
+                    <ProjectsBlock title={'Landing Page Design'} description={'Landing Page designed using Figma'}
+                                   img={picture}
+                                   size={'small'} width={'w-full'}/>
+                    <ProjectsBlock title={'Chatbot'}
+                                   description={'ChatBot developed using NLP algorithm and NLTK Tools'} img={picture}
+                                   size={'small'} width={'w-full'}/>
+                </div>
             ) : (
                 <div className={'flex flex-col gap-6'}>
                     <div className={'flex gap-8'}>
-                        <ProjectsBlock title={'Weather App'} description={'Mobile App created using Swift for weather prediction using AccuWeather API'} img={weatherApp}
+                        <ProjectsBlock title={'Weather App'}
+                                       description={'Mobile App created using Swift for weather prediction using AccuWeather API'}
+                                       img={weatherApp}
                                        size={'small'} width={'w-full'}/>
-                        <ProjectsBlock title={'Vegan Recipes'} description={'Mobile App created using Flutter to bring easily vegan recipes for users.'} img={picture}
+                        <ProjectsBlock title={'Vegan Recipes'}
+                                       description={'Mobile App created using Flutter to bring easily vegan recipes for users.'}
+                                       img={picture}
                                        size={'small'} width={'w-full'}/>
                     </div>
-                    <ProjectsBlock title={'E-Commerce Webstore'} description={'Clothing webstore made with React Typescript'} img={aguadoce}
+                    <ProjectsBlock title={'E-Commerce Webstore'}
+                                   description={'Clothing webstore made with React Typescript'} img={aguadoce}
                                    size={'large'} width={'w-full'}/>
                     <div className={'flex gap-8'}>
-                        <ProjectsBlock title={'Landing Page Design'} description={'Landing Page designed using Figma'} img={picture}
+                        <ProjectsBlock title={'Landing Page Design'} description={'Landing Page designed using Figma'}
+                                       img={picture}
                                        size={'small'} width={'w-full'}/>
-                        <ProjectsBlock title={'Vegan Recipes'} description={'Mobile App created using Flutter to bring easily vegan recipes for users.'} img={picture}
+                        <ProjectsBlock title={'Chatbot'}
+                                       description={'ChatBot developed using NLP algorithm and NLTK Tools'}
+                                       img={picture}
                                        size={'small'} width={'w-full'}/>
                     </div>
                 </div>
