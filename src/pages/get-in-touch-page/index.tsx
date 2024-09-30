9a63cf1ee4ea290096edd4b52b6a86b63ff2cab4
jpontel <jpontel2002@gmail.com>
2024-09-30 02:52:51 -0300

    get in touch improve...

--- src/pages/get-in-touch-page/index.tsx
+++ src/pages/get-in-touch-page/index.tsx
@@ -1,12 +1,27 @@
+import React, { Fragment, useEffect, useRef, useState } from "react";
 import GenericInput from '@/components/generic-input';
-import SubmitButton from "@/components/submit-button";
+import SubmitButton from '@/components/submit-button';
 import emailIcon from '@/assets/email-icon.svg';
 import phoneIcon from '@/assets/phone-icon.svg';
-import React, {Fragment, useState} from "react";
 
-function StyledDiv({message}: { message: string }) {
+function StyledDiv({ message, onClose }: { message: string, onClose: () => void }) {
+    const divRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        function handleClickOutside(event: MouseEvent) {
+            if (divRef.current && !divRef.current.contains(event.target as Node)) {
+                onClose();
+            }
+        }
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [onClose]);
+
     return (
-        <div className={'w-[300px] border border-slate-400 h-[100px] font-bold font-baijamjuree text-white bg-zinc-800 rounded-lg p-6 text-center items-center'}>
+        <div ref={divRef} className={'w-[300px] border border-slate-400 h-[100px] font-bold font-baijamjuree text-white bg-zinc-800 rounded-lg p-6 text-center items-center'}>
             {message}
         </div>
     );
@@ -23,7 +38,7 @@ export default function GetInTouchPage() {
     });
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const {name, value} = e.target;
+        const { name, value } = e.target;
         setFormData({
             ...formData,
             [name]: value
@@ -47,51 +62,51 @@ export default function GetInTouchPage() {
                 body: JSON.stringify(payload)
             });
             if (response.ok) {
-                setIsLoading(false)
+                setIsLoading(false);
                 setMessage('Form submitted successfully!');
             } else {
-                setIsLoading(false)
+                setIsLoading(false);
                 setMessage('Error');
             }
         } catch (e) {
             console.error(e);
-            setIsLoading(false)
+            setIsLoading(false);
             setMessage('Error');
         }
+    };
+
+    const handleCloseMessage = () => {
+        setMessage('');
     };
 
     return (
         <Fragment>
             {isLoading && (
-                <div className={'w-full h-full items-center justify-center absolute'}>
-                    <StyledDiv message={'Loading...'}/>
+                <div className={'w-full h-full items-center justify-center absolute flex'}>
+                    <StyledDiv message={'Loading...'} onClose={handleCloseMessage} />
                 </div>
             )}
             {message && (
-                <div className={'w-full h-full items-center justify-center absolute'}>
-                    <StyledDiv message={message}/>
+                <div className={'w-full h-full items-center justify-center absolute flex'}>
+                    <StyledDiv message={message} onClose={handleCloseMessage} />
                 </div>
             )}
-            <div
-                className={'w-full h-[80vh] flex flex-col text-white lg:px-[250px] lg:pt-[100px] font-baijamjuree '}>
+            <div className={'w-full h-[80vh] flex flex-col text-white lg:px-[250px] lg:pt-[100px] font-baijamjuree '}>
                 <div className={'w-full h-full inline-flex'}>
                     <div className={'w-full h-full flex flex-col'}>
                         <div className={'styled-text font-bold text-[60px]'}>
                             Get in touch
                         </div>
                         <p className={'text-[15px] w-2/3 leading-8'}>
-                            Have a project in your mind? Looking to partner or work together? Reach out through
-                            the form
-                            and
-                            I'll get back to you in the next 48 hours.
+                            Have a project in your mind? Looking to partner or work together? Reach out through the form and I'll get back to you in the next 48 hours.
                         </p>
                         <div className={'flex flex-col gap-5 mt-5 font-semibold'}>
                             <div className={'inline-flex'}>
-                                <img src={emailIcon} alt={'Email'} className={'mr-2'}/>
+                                <img src={emailIcon} alt={'Email'} className={'mr-2'} />
                                 <p>jpontel2002@gmail.com</p>
                             </div>
                             <div className={'inline-flex'}>
-                                <img src={phoneIcon} alt={'Phone Number'} className={'mr-2'}/>
+                                <img src={phoneIcon} alt={'Phone Number'} className={'mr-2'} />
                                 <p>(41) 98497-4864</p>
                             </div>
                         </div>
@@ -121,7 +136,7 @@ export default function GetInTouchPage() {
                             isText
                             name="message"
                         />
-                        <SubmitButton handleClick={handleSubmit}/>
+                        <SubmitButton handleClick={handleSubmit} />
                     </div>
                 </div>
             </div>
